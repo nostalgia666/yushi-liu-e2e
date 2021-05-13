64883fd1bc4666933546fcb75b286dcb1e0a96f3
Yushi Liu <Yushi@Yushis-MacBook-Pro.local>
2021-05-12 18:49:34 -0700

    OA

---
+++ cypress/integration/youtube.spec.js
@@ -0,0 +1,28 @@
+/// <reference types= "cypress" />
+
+describe('My RISE assignment', () => {
+    it('Check YouTube video title & uploader', () => {
+        // Open YouTube
+        cy.visit('https://www.youtube.com')
+        // Check whether YouTube is correctly opened in the browser
+        cy.url().should('include', 'www.youtube.com')
+
+        // Type the video title in the search box
+        cy.get('input[id=search]').type('The whole working-from-home thing — Apple')
+          // Assert the input is correct
+          .should('have.value', 'The whole working-from-home thing — Apple') 
+        // Click search button
+        cy.get('button[id=search-icon-legacy]').click()
+
+        // Open the video and wait for the page to load
+        cy.intercept('POST', '/watch?v=6_pru8U2RmM&pbj=1').as('getPage')
+        cy.get('a[title="The whole working-from-home thing — Apple"]').eq(0).click()
+        cy.wait('@getPage').its('response.statusCode').should('eq', 200)
+
+        // Assert that the title of video matches the search 
+        cy.get('div[id=info-contents]').find('h1').should('have.text', 'The whole working-from-home thing — Apple')
+        // Assert the uploader is "Apple"
+        cy.get('div[id=meta-contents]').find('div[id=upload-info]').find('a')
+          .should('have.text', 'Apple')
+    })
+})
